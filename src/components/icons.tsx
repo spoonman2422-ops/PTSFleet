18abf7dbd5aee92712e0810e6d38547dd4ddeb91
Edgardo Briol <spoonman2422@gmail.com>
2025-10-04 01:07:06 +0000

    sorry its IMG_20251004_085050.jpg

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -7,7 +7,7 @@ export function AppLogo(props: SVGProps<SVGSVGElement> & { width?: number, heigh
   return (
     <Image
       {...rest}
-      src="/IMG_0251004_085050.jpg"
+      src="/IMG_20251004_085050.jpg"
       alt="Platinum Trucking Services Logo"
       width={width}
       height={height}
