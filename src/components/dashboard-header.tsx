fdc5e867af7e330f302645fbe1da4f4e4267decf
Edgardo Briol <spoonman2422@gmail.com>
2025-11-02 13:39:40 +0000

    i do not see the toggle switch to hide the left panel

--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -3,7 +3,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 export function DashboardHeader() {
   return (
     <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-        <div className="md:hidden">
+        <div>
             <SidebarTrigger />
         </div>
         <div className="flex-1">
